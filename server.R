6ad1869a0fde6a306059a757e3658442334996b1
Alexander A. Ramírez M <alexander.ramirez@synergy-gb.com>
2017-05-30 13:45:20 -0400

    app code for main UI

--- server.R
+++ server.R
@@ -1,11 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 # Make sure a package is at least some version (only installs from CRAN)
 ensure_version <- function(pkg, ver = "0.0") {
@@ -21,4 +13,5 @@ library(shinydashboard)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+
 })
--- ui.R
+++ ui.R
@@ -20,10 +20,43 @@ ensure_version("shinydashboard", "0.5.3")
 library(shiny)
 library(shinydashboard)
 
+header<-dashboardHeader(title = "Optimización de Portafolios", titleWidth = "300px")
 
-# Define UI for application that draws a histogram
-shinyUI(dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+menu<-dashboardSidebar(sidebarMenu(
+  menuItem("Datos", tabName = "data", icon=icon("table")),
+  menuItem("Estadísticas", tabName = "stats", icon=icon("pie-chart")),
+  menuItem("Optimización", tabName = "optimize", icon=icon("area-chart")),
+  menuItem("Instrumento", tabName="individual", icon=icon("file-o")),
+  menuItem("Pares", tabName="pairs", icon=icon("files-o")),
+  menuItem("Mercado", tabName = "market", icon=icon("building"))
+))
+
+body<-dashboardBody(
+  tabItems(
+    tabItem(tabName = "data",
+            h1("Carga de datos")
+    ),
+    tabItem(tabName = "stats",
+            h1("Estadísticas del Portafolio")
+    ),
+    tabItem(tabName = "optimize",
+            h1("Optimización del Portafolio")
+    ),
+    tabItem(tabName = "individual",
+            h1("Datos del Instrumento")
+    ),
+    tabItem(tabName = "pairs",
+            h1("Comparación de instrumentos")
+    ),
+    tabItem(tabName = "market",
+            h1("Referencias del Mercado")
+    )
+  )
+)
+
+# Define UI
+shinyUI(dashboardPage(skin = "green",
+  header,
+  menu,
+  body
 ))
